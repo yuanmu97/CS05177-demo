a531fb5eb77ae84c70d93599aee1b33bc6368bc4
Hypercube <hypercube@0x01.me>
2020-06-14 20:29:49 +0800

    remove legacy frontend

--- demo/views.py
+++ demo/views.py
@@ -1,7 +1,7 @@
 import json
 
 from django.http.response import JsonResponse
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import get_object_or_404
 from django.utils.decorators import method_decorator
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
@@ -9,80 +9,11 @@ from django.views.decorators.csrf import csrf_exempt
 from demo import models
 from pmz.pmz import FaceDetection
 
-
 model_face = FaceDetection()
 
 type2level = {
     'face': 3,
 }
-
-
-class Upload(View):
-    def get(self, request):
-        return render(request, 'upload.html')
-
-    def post(self, request):
-        image = models.Image.objects.create(file=request.FILES['image'])
-        res_face = model_face.inference(img_path=image.file.path)
-        for r in res_face:
-            x1, y1, x2, y2 = r['box_points']
-            image.rect_set.create(
-                type='face',
-                left=x1,
-                top=y1,
-                right=x2,
-                bottom=y2,
-                level=type2level[r['name']],
-                description='这里有人脸',
-            )
-        return redirect('image', id=image.id)
-
-
-class Image(View):
-    def get(self, request, id):
-        image = get_object_or_404(models.Image, id=id)
-        if not image.scanned:
-            return render(request, 'scan.html', {'image': image})
-        if not image.corrected:
-            return render(request, 'correct.html', {'image': image})
-        return render(request, 'download.html', {'image': image})
-
-
-class Scan(View):
-    def post(self, request, id):
-        image = get_object_or_404(models.Image, id=id)
-        image.rect_set.create(
-            type='A',
-            description='图片的左上角容易泄露隐私',
-            top=0,
-            left=0,
-            right=100,
-            bottom=100,
-            level=1,
-        )
-        image.rect_set.create(
-            type='B',
-            description='这里更容易泄露隐私',
-            top=100,
-            left=100,
-            right=200,
-            bottom=200,
-            level=3,
-        )
-        image.scanned = True
-        image.save()
-        return redirect('image', id=image.id)
-
-
-class Correct(View):
-    def post(self, request, id):
-        image = get_object_or_404(models.Image, id=id)
-        for rect in image.rect_set.all():
-            rect.level_corrected = int(request.POST[f'{rect.id}/level'])
-            rect.save()
-        image.corrected = True
-        image.save()
-        return redirect('image', id=image.id)
 
 
 @method_decorator(csrf_exempt, name='dispatch')
@@ -94,6 +25,18 @@ class ApiImage(View):
     def post(self, request, id=None):
         if id is None:
             image = models.Image.objects.create(file=request.FILES['image'])
+            res_face = model_face.inference(img_path=image.file.path)
+            for r in res_face:
+                x1, y1, x2, y2 = r['box_points']
+                image.rect_set.create(
+                    type='face',
+                    left=x1,
+                    top=y1,
+                    right=x2,
+                    bottom=y2,
+                    level=type2level[r['name']],
+                    description='这里有人脸',
+                )
             return JsonResponse(image.json())
         else:
             image = get_object_or_404(models.Image, id=id)
@@ -104,24 +47,3 @@ class ApiImage(View):
             image.corrected = True
             image.save()
             return JsonResponse(image.json())
-
-
-@method_decorator(csrf_exempt, name='dispatch')
-class ApiRects(View):
-    def post(self, request, id):
-        image = get_object_or_404(models.Image, id=id)
-        data = json.load(request)
-        if not image.scanned:
-            for rect in data['rects']:
-                image.rect_set.create(
-                    type=rect['type'],
-                    description=rect['description'],
-                    top=rect['top'],
-                    left=rect['left'],
-                    right=rect['right'],
-                    bottom=rect['bottom'],
-                    level=rect['level'],
-                )
-            image.scanned = True
-            image.save()
-        return JsonResponse(image.json())
