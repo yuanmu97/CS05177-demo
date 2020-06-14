696be1d64e26dba915a1e9ab60da59ebd140497d
Hypercube <hypercube@0x01.me>
2020-06-14 18:26:40 +0800

    review pr

--- demo/views.py
+++ demo/views.py
@@ -7,17 +7,13 @@ from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 
 from demo import models
-
-from .pmz.pmz import FaceDetection
-from django.core.files.storage import default_storage
-from django.core.files.base import ContentFile
-import os 
+from pmz.pmz import FaceDetection
 
 
 model_face = FaceDetection()
 
 type2level = {
-    "face": 3
+    'face': 3,
 }
 
 
@@ -26,13 +22,19 @@ class Upload(View):
         return render(request, 'upload.html')
 
     def post(self, request):
-        img_file = request.FILES['image']
-        # save to var/upload/imgs
-        path = default_storage.save(f'imgs/{img_file.name}', ContentFile(img_file.read()))
-        # set img_path
-        models.Image.img_path = os.path.join('var/upload/imgs', img_file.name)
-
-        image = models.Image.objects.create(file=img_file)
+        image = models.Image.objects.create(file=request.FILES['image'])
+        res_face = model_face.inference(img_path=image.file.path)
+        for r in res_face:
+            x1, y1, x2, y2 = r['box_points']
+            image.rect_set.create(
+                type='face',
+                left=x1,
+                top=y1,
+                right=x2,
+                bottom=y2,
+                level=type2level[r['name']],
+                description='这里有人脸',
+            )
         return redirect('image', id=image.id)
 
 
@@ -49,37 +51,24 @@ class Image(View):
 class Scan(View):
     def post(self, request, id):
         image = get_object_or_404(models.Image, id=id)
-        res_face = model_face.inference(img_path=image.img_path)
-        for r in res_face:
-            x1, y1, x2, y2 = r['box_points']
-            image.rect_set.create(
-                type='Face',
-                left=x1,
-                top=y1,
-                right=x2,
-                bottom=y2,
-                level=type2level[r['name']],
-                description="这里有人脸"
-            )
-        
-        # image.rect_set.create(
-        #     type='A',
-        #     description='图片的左上角容易泄露隐私',
-        #     top=0,
-        #     left=0,
-        #     right=100,
-        #     bottom=100,
-        #     level=1,
-        # )
-        # image.rect_set.create(
-        #     type='B',
-        #     description='这里更容易泄露隐私',
-        #     top=100,
-        #     left=100,
-        #     right=200,
-        #     bottom=200,
-        #     level=3,
-        # )
+        image.rect_set.create(
+            type='A',
+            description='图片的左上角容易泄露隐私',
+            top=0,
+            left=0,
+            right=100,
+            bottom=100,
+            level=1,
+        )
+        image.rect_set.create(
+            type='B',
+            description='这里更容易泄露隐私',
+            top=100,
+            left=100,
+            right=200,
+            bottom=200,
+            level=3,
+        )
         image.scanned = True
         image.save()
         return redirect('image', id=image.id)
